2208f825a757fd2d4f85054958bc2b6de6b4695e
juanpam <jpkratos@gmail.com>
2017-11-08 08:20:12 +0000

    fixing locales

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 from app import app
 import os
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=True)
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=True)
