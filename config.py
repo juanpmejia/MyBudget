a33902b9cfaee992529ef2bf4bd2e1b4af671bd0
juanpam <jpkratos@gmail.com>
2017-11-08 08:47:31 +0000

    preparing for heroku

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 WTF_CSRF_ENABLED = True
-SECRET_KEY = "\x1c<d\xa4\xaev\x93\x93\x060~\x15 \x9f\xaf\x1bo\x87\x17\xb6\\*\xafy"
+SECRET_KEY = b'\x1c<d\xa4\xaev\x93\x93\x060~\x15 \x9f\xaf\x1bo\x87\x17\xb6\\*\xafy'
 TEMPLATES_AUTO_RELOAD = True
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
